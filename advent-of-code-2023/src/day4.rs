8a8669dcec73e5346ea7a68d5d2037ee179cceeb
Krajzys <kryzyyspl@gmail.com>
2023-12-04 19:40:59 +0100

    optimize day4 speed a bit

--- advent-of-code-2023/src/day4.rs
+++ advent-of-code-2023/src/day4.rs
@@ -36,25 +36,23 @@ pub fn day4_2() {
     }
     
     for (r, line) in games.iter().enumerate() {
-        for _ in 0..cards_dict[&r] {
-            let line_cut = String::from(&line[line.find(':').unwrap()+2..]);
-            let nums: Vec<&str> = line_cut.split('|').collect();
-            let winning_nums = nums[0].trim().replace("  ", " ");
-            let winning_nums: Vec<&str> = winning_nums.split(' ').collect();
-            let our_nums = nums[1].trim().replace("  ", " ");
-            let our_nums = our_nums.split(' ');
-            let mut wins = 0;
+        let line_cut = String::from(&line[line.find(':').unwrap()+2..]);
+        let nums: Vec<&str> = line_cut.split('|').collect();
+        let winning_nums = nums[0].trim().replace("  ", " ");
+        let winning_nums: Vec<&str> = winning_nums.split(' ').collect();
+        let our_nums = nums[1].trim().replace("  ", " ");
+        let our_nums = our_nums.split(' ');
+        let mut wins = 0;
 
-            for num in our_nums {
-                if winning_nums.contains(&num) {
-                    wins += 1;
-                }
+        for num in our_nums {
+            if winning_nums.contains(&num) {
+                wins += 1;
             }
-            for i in 1..=wins {
-                let game_no = r+i;
-                if game_no < cards_dict.len() {
-                    cards_dict.insert(game_no, cards_dict[&game_no]+1);
-                }
+        }
+        for i in 1..=wins {
+            let game_no = r+i;
+            if game_no < cards_dict.len() {
+                cards_dict.insert(game_no, cards_dict[&game_no]+cards_dict[&r]);
             }
         }
     }
